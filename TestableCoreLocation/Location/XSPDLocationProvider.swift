f1ead3ca2aac262688d9833cab18367951d5f812
Scott Moon <jiwonsis@naver.com>
2017-11-03 21:46:47 +0900

    - 위치 공급자 추가

---
+++ TestableCoreLocation/Location/XSPDLocationProvider.swift
@@ -0,0 +1,70 @@
+//
+//  XSPDLocationProvider.swift
+//  TestableCoreLocation
+//
+//  Created by Scott moon on 03/11/2017.
+//  Copyright © 2017 Scott moon. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol XSPDLocationConsumer: class {
+    func consumeLocation(_ location: CLLocation)
+}
+
+protocol XSPDLocationProvider: class {
+    func add(_ consumer: XSPDLocationConsumer)
+}
+
+class XSPDDefaultLocationProvider {
+    let locationManager: XSPDLocationManager
+    let locationAuthorization: XSPDLocationAuthorization
+    
+    var locationConsumers = [XSPDLocationConsumer]()
+    
+    init(locationManger: XSPDLocationManager, locationAuthorization: XSPDLocationAuthorization) {
+        self.locationManager = locationManger
+        self.locationAuthorization = locationAuthorization
+        
+        locationManger.delegate = self
+    }
+    
+    deinit {
+        locationManager.stopUpdatingLocation()
+    }
+}
+
+private extension XSPDDefaultLocationProvider {
+    
+    func setupNotifications() {
+        NotificationCenter.default.addObserver(forName: .XSPDLocationAuthorized,
+                                               object: locationAuthorization,
+                                               queue: .main) { [weak self](_) in
+            self?.locationManager.startUpdatingLocation()
+        }
+    }
+}
+
+extension XSPDDefaultLocationProvider: XSPDLocationProvider {
+    func add(_ consumer: XSPDLocationConsumer) {
+        locationConsumers.append(consumer)
+    }
+}
+
+extension XSPDDefaultLocationProvider: XSPDLocationManagerDelegate {
+    
+    func locationManager(_ manager: XSPDLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let sortedLocations = locations.sorted { (lhs, rhs) -> Bool in
+            return lhs.timestamp.compare(rhs.timestamp) == .orderedDescending
+        }
+        
+        guard let location = sortedLocations.first else { return }
+        for consumer in locationConsumers {
+            consumer.consumeLocation(location)
+        }
+    }
+    
+}
+
