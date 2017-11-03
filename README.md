8961c53f1e1ebf4130290c82ee52b1d2f5abebb7
Scott Moon <jiwonsis@naver.com>
2017-11-03 20:14:17 +0900

    위치 서비스 권한 관리자 업데이트

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 testable-core-location
 ======================
 
-#### 해당 브런치는 [PART5. 테스트를 돕기위해 프로토콜 사용하기 (코딩편)의 소스입니다.](https://github.com/jiwonsis/testable-core-location/commit/da56848ff3389ce78f8264ab8ade153b7d534057)
+#### 해당 브런치는 TDD기반의 스위프트 코드 개발 \#8 위치 서비스 인증 관리자 만들기 편입니다
---
+++ TestableCoreLocation/Location/XSPDLocationAuthorization.swift
@@ -0,0 +1,59 @@
+//
+//  XSPDLocationAuthorization.swift
+//  TestableCoreLocation
+//
+//  Created by Scott moon on 03/11/2017.
+//  Copyright © 2017 Scott moon. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+extension NSNotification.Name {
+    static let XSPDLocationAuthorized =
+        NSNotification.Name(rawValue: "NSNotification.Name.XSPDLocationAuthorized")
+}
+
+protocol XSPDLocationAuthorizationDelegate: class {
+    func authorizationDenied(for locationAuthorization: XSPDLocationAuthorization)
+}
+
+protocol XSPDLocationAuthorization: class {
+    var delegate: XSPDLocationAuthorizationDelegate? { get set }
+    
+    func checkAuthorization()
+}
+
+class XSPDDefaultLocationAuthorization {
+    weak var delegate: XSPDLocationAuthorizationDelegate?
+    let locationManager: XSPDLocationManager
+    
+    init(locationManger: XSPDLocationManager) {
+        self.locationManager = locationManger
+        locationManger.authorizationDelegate = self
+    }
+}
+
+extension XSPDDefaultLocationAuthorization: XSPDLocationAuthorization {
+    
+    func checkAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+    }
+}
+
+extension XSPDDefaultLocationAuthorization: XSPDLocationManagerAuthorizationDelegate {
+    func locationManager(_ manager: XSPDLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            NotificationCenter.default.post(name: .XSPDLocationAuthorized, object: self)
+        case .denied, .restricted:
+            delegate?.authorizationDenied(for: self)
+        default: break
+        }
+    }
+}
