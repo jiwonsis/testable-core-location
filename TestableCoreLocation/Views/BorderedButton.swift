d6643aada774f12e14d2977efc0a0e112ed51927
Scott Moon <jiwonsis@naver.com>
2017-11-06 17:19:50 +0900

    스토리 보드와 커스텀 버튼

---
+++ TestableCoreLocation/Views/BorderedButton.swift
@@ -0,0 +1,35 @@
+//
+//  BorderedButton.swift
+//  TestableCoreLocation
+//
+//  Created by Scott moon on 06/11/2017.
+//  Copyright © 2017 Scott moon. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class BorderedButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    func commonInit() {
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+        layer.borderWidth = 1
+        layer.borderColor = titleLabel?.textColor?.cgColor
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        commonInit()
+    }
+
+}
